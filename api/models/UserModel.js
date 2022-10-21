770c13c7502adc88f5f4cb5bacb0be4661512c6f
maabdullah511 <maabdullah511@gmail.com>
2022-10-22 00:13:26 +0600

    create model and role

--- api/models/UserModel.js
+++ api/models/UserModel.js
@@ -1,1 +1,25 @@
-// user model
+module.exports = (mongoose) => {
+    //User Schema
+    var userSchema = mongoose.Schema(
+        {
+            firstname: { type: String, require: true },
+            lastname: { type: String, require: true },
+            username: { type: String, unique: true, require: true },
+            password: { type: String, require: true },
+            roles: [
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Role"
+                }
+            ]
+        },
+        { timestamps: true }
+    )
+    userSchema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+    const User = mongoose.model('User', userSchema);
+    return User;
+}
